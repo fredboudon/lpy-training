964ef51bd3a3d059487ff2a9f8ff657fca4252a9
Frederic Boudon <frederic.boudon@cirad.fr>
2024-09-19 16:19:02 +0200

    improve project

--- project/script.py
+++ project/script.py
@@ -1,5 +1,8 @@
-from numpy import arange
+from numpy import arange, genfromtxt
 from openalea.lpy import *
+
+#params = genfromtxt("leafsize.txt", delimiter=";")
+#print(params)
 
 for leafsize in arange(0.1,2,0.1):
   l = Lsystem('example.lpy',{'leafsize':leafsize})
